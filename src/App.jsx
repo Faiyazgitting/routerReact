c8d2defcc3869f7e48ac1678a2d28663d9c2a4b5
Md Faiyazuddin <faiyazuddinmd97@gmail.com>
2024-05-03 18:56:17 +0530

    React Router project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './App.css'
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+export default App
